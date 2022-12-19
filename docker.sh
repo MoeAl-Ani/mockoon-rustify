e3007296bb1f1b66c02960f571e71a92dff6d446
Al-Ani Mohammed <mohammedalanny@gmail.com>
2022-12-19 17:21:22 +0200

    - exposed 8080 port

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
 docker build -t mockoon-rust .
 data_volume=$PWD/data
 echo $data_volume
-docker run --volume $data_volume:/data -it --rm mockoon-rust mockoon-rust --data /data/api-config.json
+docker run --volume $data_volume:/data -p8080:8080 -it --rm mockoon-rust mockoon-rust --data /data/api-config.json
